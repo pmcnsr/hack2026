32aecf0734b50866ea99d50d544b67d425feaa55
Peter Muschick <peter.muschick@consor.ch>
2026-01-30 10:06:36 +0100

    add more precise dev prompt

--- src/main/java/com/openai/hackathon/ChatController.java
+++ src/main/java/com/openai/hackathon/ChatController.java
@@ -8,8 +8,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
